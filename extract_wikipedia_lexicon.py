e30839c3b5a596aaf3d7788bb45d599e39ecca40
Juan Ignacio Navarro Horniacek <sicotronic@gmail.com>
2017-10-12 22:37:04 +0900

    unifying categories, mention and links function

--- extract_wikipedia_lexicon.py
+++ extract_wikipedia_lexicon.py
@@ -9,18 +9,11 @@ import click
 PROCESS_AMOUNT = 5
 
 
-def extract_categories(wiki_page):
-    title, _, content = wiki_page
-    if title and len(title) != 0:
-        current_uri = format_as_uri(clean_title(title))
-        categories = get_category(content)
-        return current_uri, categories
-
-
-def extract_mention_links(wiki_page):
+def extract_mention_links_categories(wiki_page):
     title, page_type, content = wiki_page
     if title and len(title) != 0:
         current_uri = format_as_uri(clean_title(title))
+        categories = get_category(content)
         if page_type == 'redirect':
             target_uri = format_as_uri(content)
             links = [target_uri]
@@ -28,20 +21,16 @@ def extract_mention_links(wiki_page):
             lexicon = [(title, target_uri, current_uri, '#REDIRECT')]
         elif content:
             lexicon, links = extract_anchor_links(current_uri, content)
-        return current_uri, lexicon, links
+        return current_uri, lexicon, links, categories
 
 
 def process_xml_wiki(xmlf):
     process_pool = Pool(processes=PROCESS_AMOUNT)
     lexicon_db = Lexicon()
-    # save the categories obtained for the current page
-    with click.progressbar(process_pool.map(extract_categories, extract_page(xmlf)),
-                           label='obtaining categories') as categories_progress_bar:
-        for source_uri, categories_list in categories_progress_bar:
-            lexicon_db.insert_categories_uri(source_uri, categories_list)
-    with click.progressbar(process_pool.map(extract_mention_links, extract_page(xmlf)),
+    with click.progressbar(process_pool.map(extract_mention_links_categories, extract_page(xmlf)),
                            label='obtaining mentions and links') as mention_link_progress_bar:
-        for source_uri, lexicon, links in mention_link_progress_bar:
+        for source_uri, lexicon, links, categories_list in mention_link_progress_bar:
+            lexicon_db.insert_categories_uri(source_uri, categories_list)
             lexicon_db.insert_links_uri(source_uri, links)
             lexicon_db.insert_mentions_uris(lexicon)
 
@@ -53,11 +42,6 @@ def main():
                              default='./data/wiki.xml',
                              help='Sets the path to wikipedia dump xml file',
                              type=str)
-    args_parser.add_argument('--article_number',
-                             dest='article_number',
-                             default=-1,
-                             help='amount of articles already processed before',
-                             type=int)
     options = args_parser.parse_args()
     # segment the text in the input files
     print("Starting the Entity Extraction process...")
